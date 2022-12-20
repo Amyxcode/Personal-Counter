07e7cb14497af22e800052d5e9e47b96f7ddc4f5
Amyxcode <107558599+Amyxcode@users.noreply.github.com>
2022-12-20 19:22:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let undoEL= document.getElementById("undo-el")
 let count = 0
 
 function increment() {
@@ -16,4 +17,12 @@ function save() {
     countEl.textContent = 0
     count = 0
 }
+
+function undo () {
+    let undo = count - 1
+    countEl.textContent=0
+    saveEl.textContent -= CountStr
+    count=0
+}
+
 
